fa790c4b1829ecaa36b9d313f259d069968b0152
Bahaa-fahim <bhafhym@gmail.com>
2025-06-23 23:27:46 +0300

    Fix vite base config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- base: '/Employee-Management-System/',
+//  base: '/Employee-Management-System/',
 
  
   build: {
